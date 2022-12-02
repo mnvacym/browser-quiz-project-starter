f31a72f63299816fdc460857482c891963a185f6
Andrei <a.g.popov@hotmail.com>
2022-12-02 14:31:51 +0100

    keeping the same page has color of answered question and alert if try to answer again result of answers saving in dataQuiz file

--- src/pages/questionPage.js
+++ src/pages/questionPage.js
@@ -23,12 +23,27 @@ export const initQuestionPage = () => {
 
   userInterface.appendChild(questionElement);
 
+  if (currentQuestion.selected === true) {
+    document.querySelector('h1').style.color = 'green';
+  } else if (currentQuestion.selected === false) {
+    document.querySelector('h1').style.color = 'red';
+  }
+
   const answersListElement = document.getElementById(ANSWERS_LIST_ID);
 
   for (const [key, answerText] of Object.entries(currentQuestion.answers)) {
     const answerElement = createAnswerElement(key, answerText);
     answersListElement.appendChild(answerElement);
     answerElement.addEventListener('click', () => {
+      if (currentQuestion.selected === true) {
+        alert(
+          'You already answered this question! ANSWER WAS CORRECT. Please answer the next question.'
+        );
+      } else if (currentQuestion.selected === false) {
+        alert(
+          'You already answered this question! ANSWER WAS WRONG. Please answer the next question.'
+        );
+      }
       showCorrectAnswer(answerElement, key, currentQuestion);
       sessionStorage.setItem('quizDataSaved', JSON.stringify(quizData));
     });
