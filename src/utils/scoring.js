a71682cd28029ea9659b2ef9ff6a845afe8c15df
intruston <intrustonly@gmail.com>
2022-12-02 23:52:07 +0100

    Updated show answer & CSS styling
    Some bugfixes for showCorrectAnswerUtil.js
    And CSS styling updates.

--- src/utils/scoring.js
+++ src/utils/scoring.js
@@ -2,15 +2,14 @@ let newScore = 0;
 export function scoring() {
   let scorePerQuestion = 0;
   const score = document.createElement('p');
-  score.textContent = 'Your Score is: ' + scorePerQuestion;
+  score.textContent = 'Your score: ' + scorePerQuestion;
   score.id = 'score';
   document.body.appendChild(score);
-  score.className = 'banner-correct';
 }
 
 export function newScoring() {
   newScore += 1;
 
   const scoreElement = document.getElementById('score');
-  scoreElement.innerHTML = 'Your Score is: ' + newScore;
+  scoreElement.innerHTML = 'Your score: ' + newScore;
 }
